89c07c315c9267ada66711f01bcb39e51053ae9b
Kasapov Dmitry <dkasapov@mail.ru>
2023-01-15 03:06:16 +0300

    Delete kursJS directory

--- kursJS/tasks/task3.js
+++
@@ -1,9 +0,0 @@
-// Задание 3
-// Дана строка. Необходимо вывести в консоль перевёрнутый вариант. Например, "Hello" -> "olleH".
-let answer3 = document.querySelector('.answer3');
-let btn3 = document.querySelector('.btn3');
-function func3() {
-    let result = answer3.innerHTML.split('').reverse().join('');
-    answer3.innerHTML = result;
-}
-btn3.onclick = func3;
--- kursJS/tasks/task4.js
+++
@@ -1,9 +0,0 @@
-// Задание 4
-// Записать в переменную случайное целое число в диапазоне [0; 100]. Используйте объект Math.
-let answer4 = document.querySelector('.answer4');
-let btn4 = document.querySelector('.btn4');
-function func4() {
-    let result = Math.round(Math.random()*100);
-    answer4.innerHTML = result;
-}
-btn4.onclick = func4;
